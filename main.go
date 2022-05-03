6145006cca8263f0e0621063ed19b1f378638681
liao qiulin <1286936364@qq.com>
2022-05-03 17:11:50 +0800

    variable

--- main.go
+++ main.go
@@ -3,13 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	// 值类型
-	fmt.Println("go" + "lang")
 
-	fmt.Println("1+1 =", 1+1)
-	fmt.Println("7.0/3.0 =", 7.0/3.0)
+	var a = "initial"
+	fmt.Println(a)
 
-	fmt.Println(true && false)
-	fmt.Println(true || false)
-	fmt.Println(!true)
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "apple"
+	fmt.Println(f)
+
+	var g bool
+	fmt.Println(g)
+
 }
