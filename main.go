f3a285e2e47923f9e22480e82e72acf582486004
liao qiulin <1286936364@qq.com>
2022-05-04 22:36:04 +0800

    Methods

--- main.go
+++ main.go
@@ -2,36 +2,30 @@ package main
 
 import "fmt"
 
-type person struct {
-	name string
-	age  int
+type rect struct {
+	width, height int
 }
 
-func newPerson(name string) *person {
+// 此 area 方法的接收器类型为 *rect。
+func (r *rect) area() int {
+	return r.width * r.height
+}
 
-	p := person{name: name}
-	p.age = 42
-	return &p
+// 可以为 指针或值接收类型 定义方法。这是一个值接收的示例。
+func (r rect) perim() int {
+	return 2*r.width + 2*r.height
 }
 
 func main() {
+	r := rect{width: 10, height: 5}
 
-	fmt.Println(person{"Bob", 20})
+	// 这里我们调用为我们的结构定义的 2 个方法。
+	fmt.Println("area: ", r.area())
+	fmt.Println("perim:", r.perim())
 
-	fmt.Println(person{name: "Alice", age: 30})
-
-	fmt.Println(person{name: "Fred"})
-
-	fmt.Println(&person{name: "Ann", age: 40})
-
-	fmt.Println(newPerson("Jon"))
-
-	s := person{name: "Sean", age: 50}
-	fmt.Println(s.name)
-
-	sp := &s
-	fmt.Println(sp.age)
-
-	sp.age = 51
-	fmt.Println(sp.age)
+	// Go 自动处理方法调用的值和指针之间的转换。
+	// 您可能希望使用指针接收器类型来避免复制方法调用或允许方法改变接收结构。
+	rp := &r
+	fmt.Println("area: ", rp.area())
+	fmt.Println("perim:", rp.perim())
 }
