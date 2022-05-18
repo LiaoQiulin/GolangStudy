2daaaf2302ac6db8358c0f4dc9b044c5f643f636
liao qiulin <1286936364@qq.com>
2022-05-19 00:12:20 +0800

    Timeouts

--- main.go
+++ main.go
@@ -5,30 +5,38 @@ import (
 	"time"
 )
 
-// Go 的 select 让你等待多个通道操作。将 goroutine 和通道与 select 相结合是 Go 的一个强大功能。
+// 超时对于连接到外部资源或需要限制执行时间的程序很重要。多亏了 channels 和 select ，在 Go 中实现超时变得简单而优雅。
 func main() {
 
-	// 对于我们的示例，我们将跨两个通道进行选择。
-	c1 := make(chan string)
-	c2 := make(chan string)
-
-	// 每个通道都会在一段时间后收到一个值，以模拟例如阻塞在并发 goroutine 中执行的 RPC 操作。
-	go func() {
-		time.Sleep(1 * time.Second)
-		c1 <- "one"
-	}()
+	// 对于我们的示例，假设我们正在执行一个外部调用，该调用在 2 秒后在通道 c1 上返回其结果。
+	// 请注意，通道是缓冲的，因此 goroutine 中的发送是非阻塞的。
+	// 这是防止 goroutine 泄漏的常见模式，以防从不读取通道。
+	c1 := make(chan string, 1)
 	go func() {
 		time.Sleep(2 * time.Second)
-		c2 <- "two"
+		c1 <- "result 1"
 	}()
 
-	// 我们将使用 select 同时等待这两个值，并在每个值到达时打印它们。
-	for i := 0; i < 2; i++ {
-		select {
-		case msg1 := <-c1:
-			fmt.Println("received", msg1)
-		case msg2 := <-c2:
-			fmt.Println("received", msg2)
-		}
+	// 这是实现超时的 select。
+	// res := <-c1 等待结果，<-time.After 等待超时 1s 后发送的值。
+	// 由于 select 会处理第一个准备好的接收，因此如果操作花费的时间超过允许的 1，我们将采用超时 case 。
+	select {
+	case res := <-c1:
+		fmt.Println(res)
+	case <-time.After(1 * time.Second):
+		fmt.Println("timeout 1")
+	}
+
+	// 如果我们允许更长的超时时间为 3 秒，那么来自 c2 的接收将成功，我们将打印结果。
+	c2 := make(chan string, 1)
+	go func() {
+		time.Sleep(2 * time.Second)
+		c2 <- "result 2"
+	}()
+	select {
+	case res := <-c2:
+		fmt.Println(res)
+	case <-time.After(3 * time.Second):
+		fmt.Println("timeout 2")
 	}
 }
