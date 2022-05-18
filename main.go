0863e01c814a83c6ef73f3aaafbd869949bb4971
liao qiulin <1286936364@qq.com>
2022-05-18 23:21:05 +0800

    Channel Buffering

--- main.go
+++ main.go
@@ -4,17 +4,16 @@ import "fmt"
 
 func main() {
 
-	// 通道是连接并发 goroutine 的管道。您可以将值从一个 goroutine 发送到通道，然后将这些值接收到另一个 goroutine。
+	// 默认情况下，通道是无缓冲的，这意味着如果有相应的接收 (<- chan) 准备好接收发送的值，它们将只接受发送 (chan <-)。缓冲通道接受有限数量的值，而这些值没有相应的接收器。
 
-	// 使用 make(chan val-type) 创建一个新频道。通道按它们传达的值进行分类。
-	messages := make(chan string)
+	// 在这里，我们创建了一个字符串通道，最多可缓冲 2 个值。
+	messages := make(chan string, 2)
 
-	// 使用通道 <- 语法将值发送到通道。在这里，我们从一个新的 goroutine 向我们上面创建的消息通道发送“ping”。
-	go func() { messages <- "ping" }()
+	// 因为这个通道是缓冲的，我们可以将这些值发送到通道中，而无需相应的并发接收。
+	messages <- "buffered"
+	messages <- "channel"
 
-	// <-channel 语法从通道接收一个值。在这里，我们将收到我们上面发送的“ping”消息并将其打印出来。
-	msg := <-messages
-	fmt.Println(msg)
-
-	// 默认情况下，发送和接收会阻塞，直到发送方和接收方都准备好。该属性允许我们在程序结束时等待“ping”消息，而无需使用任何其他同步
+	// 稍后我们可以像往常一样接收这两个值。
+	fmt.Println(<-messages)
+	fmt.Println(<-messages)
 }
