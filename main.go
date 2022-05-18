c474a7b9fc6844a60c3c55485a82bfcf6148818a
liao qiulin <1286936364@qq.com>
2022-05-19 00:19:58 +0800

    Non-Blocking Channel Operations

--- main.go
+++ main.go
@@ -1,42 +1,41 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
-// 超时对于连接到外部资源或需要限制执行时间的程序很重要。多亏了 channels 和 select ，在 Go 中实现超时变得简单而优雅。
+// 通道上的基本发送和接收是阻塞的。
+// 但是，我们可以使用带有 default 子句的 select 来实现非阻塞发送、接收，甚至是非阻塞多路选择。
 func main() {
+	messages := make(chan string)
+	signals := make(chan bool)
 
-	// 对于我们的示例，假设我们正在执行一个外部调用，该调用在 2 秒后在通道 c1 上返回其结果。
-	// 请注意，通道是缓冲的，因此 goroutine 中的发送是非阻塞的。
-	// 这是防止 goroutine 泄漏的常见模式，以防从不读取通道。
-	c1 := make(chan string, 1)
-	go func() {
-		time.Sleep(2 * time.Second)
-		c1 <- "result 1"
-	}()
-
-	// 这是实现超时的 select。
-	// res := <-c1 等待结果，<-time.After 等待超时 1s 后发送的值。
-	// 由于 select 会处理第一个准备好的接收，因此如果操作花费的时间超过允许的 1，我们将采用超时 case 。
+	// 这是一个非阻塞接收。
+	// 如果消息上有可用的值，则 select 将采用具有该值的 <-messages 大小写。
+	// 如果不是，它将立即采用默认情况。
 	select {
-	case res := <-c1:
-		fmt.Println(res)
-	case <-time.After(1 * time.Second):
-		fmt.Println("timeout 1")
+	case msg := <-messages:
+		fmt.Println("received message", msg)
+	default:
+		fmt.Println("no message received")
 	}
 
-	// 如果我们允许更长的超时时间为 3 秒，那么来自 c2 的接收将成功，我们将打印结果。
-	c2 := make(chan string, 1)
-	go func() {
-		time.Sleep(2 * time.Second)
-		c2 <- "result 2"
-	}()
+	// 非阻塞发送的工作方式类似。
+	// 这里 msg 不能发送到消息通道，因为通道没有缓冲区，也没有接收器。因此选择默认情况。
+	msg := "hi"
 	select {
-	case res := <-c2:
-		fmt.Println(res)
-	case <-time.After(3 * time.Second):
-		fmt.Println("timeout 2")
+	case messages <- msg:
+		fmt.Println("sent message", msg)
+	default:
+		fmt.Println("no message sent")
+	}
+
+	// 我们可以在 default 子句之上使用多种情况来实现多路非阻塞选择。
+	// 在这里，我们尝试对消息和信号进行非阻塞接收。
+	select {
+	case msg := <-messages:
+		fmt.Println("received message", msg)
+	case sig := <-signals:
+		fmt.Println("received signal", sig)
+	default:
+		fmt.Println("no activity")
 	}
 }
