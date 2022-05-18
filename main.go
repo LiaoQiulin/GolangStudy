4f1a884e7c2453250b5c9654d319757de34b7aa9
liao qiulin <1286936364@qq.com>
2022-05-19 01:17:12 +0800

    Tickers

--- main.go
+++ main.go
@@ -5,30 +5,28 @@ import (
 	"time"
 )
 
-// 我们经常希望在未来的某个时间点执行 Go 代码，或者在某个时间间隔重复执行。
-// Go 的内置 timer  和 ticker 功能使这两项任务变得简单。
-// 我们将首先学习 timer  ，后面再学习 ticker。
+// ticker适用于您想定期重复做某事。这是一个周期性滴答的代码示例，直到我们停止它
 func main() {
 
-	// 计时器代表未来的单个事件。您告诉计时器您要等待多长时间，它提供了一个届时将通知的通道。此计时器将等待 2 秒。
-	timer1 := time.NewTimer(2 * time.Second)
+	// Tickers 使用与计时器类似的机制：发送值的通道。
+	// 在这里，我们将使用通道上的 select 内置函数来等待每 500 毫秒到达的值。
+	ticker := time.NewTicker(500 * time.Millisecond)
+	done := make(chan bool)
 
-	// <-timer1.C 在定时器的通道 C 上阻塞，直到它发送一个指示定时器触发的值。
-	<-timer1.C
-	fmt.Println("Timer 1 fired")
-
-	// 如果您只是想等待，您可以使用 time.Sleep。
-	// 计时器可能有用的一个原因是您可以在计时器触发之前取消它。这是一个例子。
-	timer2 := time.NewTimer(time.Second)
 	go func() {
-		<-timer2.C
-		fmt.Println("Timer 2 fired")
+		for {
+			select {
+			case <-done:
+				return
+			case t := <-ticker.C:
+				fmt.Println("Tick at", t)
+			}
+		}
 	}()
-	stop2 := timer2.Stop()
-	if stop2 {
-		fmt.Println("Timer 2 stopped")
-	}
 
-	// 给 timer2 足够的时间来触发它，如果它会触发的话，以表明它实际上已经停止了
-	time.Sleep(3 * time.Second)
+	// ticker 可以像计时器一样停止。一旦代码停止，它将不会在其通道上接收任何值。我们将在 1600 毫秒后停止。
+	time.Sleep(1600 * time.Millisecond)
+	ticker.Stop()
+	done <- true
+	fmt.Println("Ticker stopped")
 }
