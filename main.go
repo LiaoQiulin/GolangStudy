8dda64ac2953d620a03867cb0c5c2237239f9fc2
liao qiulin <1286936364@qq.com>
2022-05-18 22:39:19 +0800

    Goroutines

--- main.go
+++ main.go
@@ -1,65 +1,35 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"time"
 )
 
-// 在 Go 中，通过显式的、单独的返回值来传达错误是惯用的。
-
-// 按照惯例，errors 是最后一个返回值，并且具有 error 类型，一个内置接口。
-func f1(arg int) (int, error) {
-	if arg == 42 {
-
-		// errors.New 使用给定的错误消息构造一个基本错误值。
-		return -1, errors.New("can't work with 42")
-
+func f(from string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(from, ":", i)
+		time.Sleep(time.Microsecond)
 	}
-
-	// 错误位置的 nil 值表示没有错误。
-	return arg + 3, nil
-}
-
-// 通过在自定义类型上实现 Error() 方法，可以将自定义类型用作 error 。这是上面示例的一个变体，它使用自定义类型来显式表示参数错误。
-type argError struct {
-	arg  int
-	prob string
-}
-
-func (e *argError) Error() string {
-	return fmt.Sprintf("%d - %s", e.arg, e.prob)
-}
-
-func f2(arg int) (int, error) {
-	if arg == 42 {
-
-		// 在这种情况下，我们使用 &argError 语法来构建一个新结构，为两个字段 arg 和 prob 提供值。
-		return -1, &argError{arg, "can't work with it"}
-	}
-	return arg + 3, nil
 }
 
 func main() {
 
-	for _, i := range []int{7, 42} {
-		if r, e := f1(i); e != nil {
-			fmt.Println("f1 failed:", e)
-		} else {
-			fmt.Println("f1 worked:", r)
-		}
-	}
-	for _, i := range []int{7, 42} {
-		if r, e := f2(i); e != nil {
-			fmt.Println("f2 failed:", e)
-		} else {
-			fmt.Println("f2 worked:", r)
-		}
-	}
+	// 假设我们有一个函数调用 f(s)。这是我们如何以通常的方式调用它，同步运行它。
+	f("direct")
 
-	// 如果您想以编程方式使用自定义错误中的数据，则需要通过类型断言将错误作为自定义错误类型的实例。
-	_, e := f2(42)
-	if ae, ok := e.(*argError); ok {
-		fmt.Println(ae.arg)
-		fmt.Println(ae.prob)
-	}
+	// 要在 goroutine 中调用此函数，请使用 go f(s)。这个新的 goroutine 将与调用的 goroutine 同时执行。
+	go f("goroutine")
+
+	// 您还可以为匿名函数调用启动一个 goroutine。
+	go func(msg string) {
+		fmt.Println(msg)
+	}("going")
+
+	fmt.Println("sleep")
+	// 我们的两个函数调用现在在不同的 goroutine 中异步运行。等待他们完成
+	time.Sleep(time.Second)
+	fmt.Println("done")
+
+	// 我们运行这个程序时，我们首先看到阻塞调用的输出，然后是两个 goroutine 的输出。
+	// goroutines 的输出可能是交错的，因为 goroutines 是由 Go 运行时并发运行的。
 }
