436f94927ad47fc7fa6aa7c18d2c7cc8edc1b1b5
yckj3231 <liaoqiulin@cloudwalk.com>
2022-05-20 17:43:05 +0800

    Epoch

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Time
+# Epoch
--- main.go
+++ main.go
@@ -5,48 +5,17 @@ import (
 	"time"
 )
 
-// Go 为时间和持续时间提供广泛的支持；这里有些例子。
+// 程序中的一个常见要求是获取自 Unix 纪元以来的秒数、毫秒数或纳秒数。以下是如何在 Go 中执行此操作。
 func main() {
-	p := fmt.Println
 
-	// 我们将从获取当前时间开始。
 	now := time.Now()
-	p(now)
+	fmt.Println(now)
 
-	// 您可以通过提供年、月、日等来构建时间结构。时间总是与位置相关联，即时区。
-	then := time.Date(
-		2009, 11, 17, 20, 34, 58, 651387237, time.UTC)
-	p(then)
+	fmt.Println(now.Unix())
+	fmt.Println(now.UnixMilli())
+	fmt.Println(now.UnixNano())
 
-	// 您可以按预期提取时间值的各个分量。
-	p(then.Year())
-	p(then.Month())
-	p(then.Day())
-	p(then.Hour())
-	p(then.Minute())
-	p(then.Second())
-	p(then.Nanosecond())
-	p(then.Location())
-
-	// 周一至周日工作日也可用。
-	p(then.Weekday())
-
-	// 这些方法比较两个时间，分别测试第一次发生在第二次之前、之后还是同时发生。
-	p(then.Before(now))
-	p(then.After(now))
-	p(then.Equal(now))
-
-	// Sub 方法返回一个 Duration 表示两次之间的间隔。
-	diff := now.Sub(then)
-	p(diff)
-
-	// 我们可以以各种单位计算持续时间的长度。
-	p(diff.Hours())
-	p(diff.Minutes())
-	p(diff.Seconds())
-	p(diff.Nanoseconds())
-
-	// 您可以使用 Add 将时间提前一个给定的持续时间，或者使用 负号 将时间向后移动一个持续时间。
-	p(then.Add(diff))
-	p(then.Add(-diff))
+	// 您还可以将自纪元以来的整数秒或纳秒转换为相应的时间。
+	fmt.Println(time.Unix(now.Unix(), 0))
+	fmt.Println(time.Unix(0, now.UnixNano()))
 }
